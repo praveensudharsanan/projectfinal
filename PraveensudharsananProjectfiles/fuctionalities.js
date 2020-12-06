17baa30a204cb3ce16792b49cf55304322f64d8a
praveensudharsanan <71019578+praveensudharsanan@users.noreply.github.com>
2020-12-06 17:33:22 +0530

    two functionalities added

---
+++ PraveensudharsananProjectfiles/fuctionalities.js
@@ -0,0 +1,39 @@
+   // accordian scripts 
+   var acc = document.getElementsByClassName("accordion");
+   var i;
+
+   for (i = 0; i < acc.length; i++) {
+     acc[i].addEventListener("click", function () {
+       this.classList.toggle("active");
+       var panel = this.nextElementSibling;
+       if (panel.style.display === "block") {
+         panel.style.display = "none";
+       } else {
+         panel.style.display = "block";
+       }
+     });
+   }
+
+   //scroll top script
+
+   var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   mybutton.style.display = "block";
+ } else {
+   mybutton.style.display = "none";
+ }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+}
+
+//pre loader
+window.onload = function(){ document.querySelector(".preloader").style.display = "none"; }
